e65651272575c6737d5b47e03455344fa7269297
Sheril J. Subramanian <sheril@Sherils-MacBook-Pro.local>
2022-07-21 16:05:59 -0400

    Dynamic URL Field

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react'
+import { TextInput } from '@contentstack/venus-components'
+import ContentstackAppSdk from '@contentstack/app-sdk'
+
+const PRODUCT = 'product'
+const FIELDS = Object.freeze({
+  title: 'title',
+  category: 'category',
+  dynamic_url: 'dynamic_url',
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState<any>(null)
+  const [app, setApp] = useState({} as any)
+  const [url, setUrl] = useState('')
+
+  const initializeApp = useCallback(async () => {
+    if (app) {
+      const customField = await app?.location?.CustomField
+      const entry = customField?.entry
+
+      setUrl(entry.getData()?.[FIELDS.dynamic_url])
+      customField.frame.updateHeight(30)
+
+      entry.onChange((data: any) => {
+        const url = constructUrl(data)
+        setUrl(url)
+
+        customField?.field?.setData(url)
+        entry.getField('url')?.setData(url)
+      })
+    }
+  }, [app])
+
+  const constructUrl = (data: any) => {
+    const productName = data?.[FIELDS.title] ?? ''
+    const formattedProductName = productName.split(' ').join('-').toLowerCase()
+    const category = data?.[FIELDS.category] ?? ''
+    return `/${PRODUCT}/${category}/${formattedProductName}`
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line no-restricted-globals
+    if (self === top) {
+      const errorMessage = 'This extension can only be used in Contentstack'
+      setError(errorMessage)
+    } else {
+      ContentstackAppSdk.init().then((appSdk) => {
+        setApp(appSdk)
+        initializeApp()
+      })
+    }
+  }, [initializeApp])
+
+  return error ? (
+    <h3>{error}</h3>
+  ) : (
+    <TextInput type="text" disabled value={url} />
+  )
 }
 
-export default App;
+export default App
